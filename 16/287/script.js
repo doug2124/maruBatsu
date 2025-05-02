3c448667734097c27e3234234715d7b2b28f8e45
c250368 <c250368@ccg.ac.jp>
2025-05-02 15:15:41 +0900

    16

--- 16/287/script.js
+++
@@ -1,9 +0,0 @@
-let a = document.querySelector("footer a");
-
-console.log(a.getAttribute('href'));
-
-let link = 'https://app.horadecodar.com.br';
-
-a.setAttribute('href',link);
-
-console.log(a.getAttribute('href'));
--- 16/293/script.js
+++
@@ -1,7 +0,0 @@
-let a = document.querySelector('#メインタイトル');
-
-setTimeout(function(){
-    a.style.color = 'red';
-    a.style.width = '400px';
-    a.style.backgroundColor = 'yellow';
-},1000);
